62105005ab704ce8804ce1b0271e1ab532510a69
Suman Yadav <sumanydv615@gmail.com>
2025-11-08 23:43:26 -0600

    starting with frontend

---
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+
+
+function LandingPage() {
+    return (
+        <>
+            <div className="flex flex-cols ml-3 items-center bg-green-100">
+                <div className="text-3xl font-semibold cursor-pointer">Sound Sage</div>
+                <div className="font-bold cursor-pointer ml-[70%]">About</div>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
